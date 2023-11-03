64bf73177faaa813ffbcaf9881118128a7d9366f
Yaroslav Sokolyansky <93289052+Sokolyanskyi@users.noreply.github.com>
2023-11-04 01:22:30 +0200

    useLocation

--- PetProjec_Store/pizza-app/src/layout/layout/Layout.tsx
+++ PetProjec_Store/pizza-app/src/layout/layout/Layout.tsx
@@ -1,8 +1,17 @@
-import {Link, Outlet} from 'react-router-dom';
+import {Link, Outlet, useLocation} from 'react-router-dom';
 import styles from './Layout.module.css';
 import Button from '../../components/Button/Button.tsx';
+import {useEffect} from 'react';
+import cn from 'classnames';
+
+cn;
 
 export function Layout() {
+	const location = useLocation();
+	useEffect(() => {
+		console.log(location);
+	}, [location]);
+
 	return <div className={styles['layout']}>
 		<div className={styles['sidebar']}>
 			<div className={styles['user']}>
@@ -13,7 +22,9 @@ export function Layout() {
 			</div>
 			<div className={styles['menu']}>
 
-				<Link to="/" className={styles['link']}>
+				<Link to="/" className={cn(styles['link'], {
+					[styles.active]: location.pathname === '/'
+				})}>
 					<img src="/menu-icon.svg" alt="menu-icon"/>
 					Menu</Link>
 				<Link to="/cart" className={styles['link']}>
